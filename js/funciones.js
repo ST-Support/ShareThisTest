01fe5c714ee24c245ee9b4cae4f0b58559c3c54a
Brenedin Gomez <brenedin.gomez@protonmail.com>
2022-04-07 07:24:28 -0600

    script en customSticky

--- js/funciones.js
+++ js/funciones.js
@@ -1,6 +1,33 @@
-var botonCorreo = document.querySelectorAll('[data-network="email"]');
 
-botonCorreo.onclick= function emailclick(){
-    window.location = "mailto:?subject=I'd like to share a link with you";
-    console.log("holas");
-}
+window.onload('inline-share-buttons', {
+    alignment: 'right',
+    id: 'share-buttons',
+    enabled: true,
+    font_size: 12,
+    padding: 3,
+    radius: 2,
+    networks: ['facebook', 'twitter', 'reddit',   'email', 'sharethis'],  
+    size: 18,
+    show_mobile_buttons: true,
+    spacing: 4,
+    onLoad: function () {
+      //override the default email sharing functionality since it's broken in outlook
+      document.querySelector('.st-btn[data-network=email]').addEventListener('click', function (e) {
+        var subject = "I'd like to share a link with you";
+        var body = document.querySelector('#share-buttons').data('url');
+        document.location = "mailto:?subject=" + subject + "&body=" + body;
+  
+        //Prevent default share this functionality
+        e.stopPropagation();
+      });
+    }
+
+    /*botonCorreo.onclick= function overrideShareSendMail() {
+    $('div[data-network="email"]').replaceWith($('div[data-network="email"]').clone());
+    $('div[data-network="email"]').on("click", function () {
+        var emailSubject = "Pyramide";
+        var emailBody = window.location.href;
+        window.location.href = "mailto:?subject=" + emailSubject + "&body=" + emailBody;
+    });
+    }
+  */
