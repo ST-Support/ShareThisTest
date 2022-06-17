a98ae6f54157075728aa65b3ca43b0867660a28e
Brenedin Gomez <brenedin.gomez@protonmail.com>
2022-06-17 08:38:41 -0600

    Email override 7

--- js/funciones.js
+++ js/funciones.js
@@ -93,7 +93,8 @@ window.__sharethis__.load('inline-share-buttons', {
   spacing: 0, // spacing = 8, no spacing = 0.
   onLoad: function () {
     //override the default email sharing functionality since it's broken in outlook
-    document.querySelector('div[data-network="email"]').replaceChild(document.querySelector('div[data-network="email"]'),document.querySelector('div[data-network="email"]'));
+    var btnEmail = document.querySelector('div[data-network="email"]');
+    btnEmail.replaceChild(btnEmail,document.querySelector('div[data-network="email"]'));
     document.querySelector('div[data-network="email"]').addEventListener("click", (e) => {
       var subject = "I'd like to share a link with you";
       var body = window.location.href;
